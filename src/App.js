6c16b279e51661155e50b61a7c7c106c575c04d8
Nynke Lys de Boer <n-deboer@outlook.com>
2018-08-20 15:16:13 +0200

    Improve infowindow

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+// INSIDE COMPONENT, you can't say 'function functionName() { ... }', instead write like this:
+// functionName = () => { ... }
+
 class App extends Component {
 
   state = {
@@ -13,20 +16,21 @@ class App extends Component {
     this.getVenues()
   }
 
-  getVenues = () => {
+  getVenues = () => { // Get venues is GET request from Foursquare
     const endPoint = "https://api.foursquare.com/v2/venues/explore?"
     const parameters = {
       client_id: "ORY3CXCT1M3CBRNVOZDJMAN250AMDHL5H24RWLMO4NYQOYVL",
       client_secret: "WU5OUOY1WL2O4JFDWKHSKDPF3OC2VXCQTLBEPNEN511AFPWD",
       query: "food",
       ll: "52.5058773, 13.4674052",
-      v: "20180323"
+      v: "20180323",
+      limit: 10
     }
 
     axios.get(endPoint + new URLSearchParams(parameters)) // Axios is comparable to fetch API
       .then(response => {
         this.setState({
-          venues: response.data.response.groups[0].items // Array of objects with places
+          venues: response.data.response.groups[0].items // Array of objects with venue data
         }, this.renderMap()) // Callback function: when response is reached and saved, render map
       })
       .catch(error => {
@@ -36,24 +40,29 @@ class App extends Component {
 
   renderMap = () => { // Function will only be invoked when we have venues
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAPWE9q9dv42yCFbSvSJBK8x8wgjrwAMrA&v=3&callback=initMap")
-    window.initMap = this.initMap // Refers to initMap function below
+    window.initMap = this.initMap // Respectively refers to initMap function below and to initMap function in callback of script URL
   }
 
-  // Inside component, you can't say 'function functionName', instead write like this:
   initMap = () => { // Add function to load map after page loads/before user interacts with map
-
-    // Constructor creates a new map instance - only center and zoom are required. First specification is where in page to load map (the map div), second is what part of world to show. Zoom can go up to 21.
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 52.5058773, lng: 13.4674052},
+    const map = new window.google.maps.Map(document.getElementById('map'), { // Initialize
+      center: {lat: 52.5058773, lng: 13.4674052}, // What location to center
       zoom: 15
    });
 
-    var infowindow = new window.google.maps.InfoWindow(); // Create single infowindow
+    const infowindow = new window.google.maps.InfoWindow(); // Create single infowindow
 
     this.state.venues.map(myVenue => { // Loop over venues inside state
-      var contentString = `${myVenue.venue.name}`;
-
-      var marker = new window.google.maps.Marker({ // Create marker
+      const contentString =
+        `${myVenue.venue.name}
+        <br>
+        ${myVenue.venue.categories[0].name}
+        <br>
+        <br>
+        ${myVenue.venue.location.formattedAddress[0]},
+        ${myVenue.venue.location.formattedAddress[1]}
+        <br>
+        (${myVenue.venue.location.distance} m away)`;
+      const marker = new window.google.maps.Marker({ // Create marker
         position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng}, // Where marker should appear
         map: map, // The map it should appear on
         title: myVenue.venue.name // Appears when hovering over (optional)
